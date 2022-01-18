e1f234662cf5571badffb524b706a739bfe7f5a8
Ayush Mehar <ayushm19100@iiitnr.edu.in>
2022-01-19 00:49:18 +0530

    Fixed collapsed cart bug

--- client/src/components/PizzaContainer/pizzaContainer.jsx
+++ client/src/components/PizzaContainer/pizzaContainer.jsx
@@ -3,7 +3,7 @@ import PizzaCard from "../PizzaCard/pizzaCard"
 
 const PizzaContainer = ({pizzas})=>{
     return (
-      <div className="row">
+      <div className="row w-100">
         {pizzas.map((pizza, i) => <PizzaCard pizza={pizza} key={i}/>)}
       </div>
     )
